bdd79cb6e58d7d81b43ebf04b6749968a69aaebd
cakuakz <rafiirvansyah88@gmail.com>
2024-11-21 00:51:59 +0700

    feat: initial commit

---
+++ rafi.blogspot/src/utils/static.ts
@@ -0,0 +1,30 @@
+import type { BlogCardProps } from "./types";
+
+export const CloudinaryBaseUrl = "https://asset.cloudinary.com/"
+
+export const BlogCoverStatic: BlogCardProps[] = [
+    {
+        title: "What is React.Js?",
+        description: "Here's to know more about what React.Js actually is",
+        imgUrl: "https://res.cloudinary.com/rafirfansyah/image/upload/v1731826909/React_Image_s6nsdn.png",
+        duration: "5 Minutes"
+    },
+    {
+        title: "What is React.Js?",
+        description: "Here's to know more about what React.Js actually is",
+        imgUrl: "https://res.cloudinary.com/rafirfansyah/image/upload/v1731826909/React_Image_s6nsdn.png",
+        duration: "5 Minutes"
+    },
+    {
+        title: "What is React.Js?",
+        description: "Here's to know more about what React.Js actually is",
+        imgUrl: "https://res.cloudinary.com/rafirfansyah/image/upload/v1731826909/React_Image_s6nsdn.png",
+        duration: "5 Minutes"
+    },
+    {
+        title: "What is React.Js?",
+        description: "Here's to know more about what React.Js actually is",
+        imgUrl: "https://res.cloudinary.com/rafirfansyah/image/upload/v1731826909/React_Image_s6nsdn.png",
+        duration: "5 Minutes"
+    }
+]
