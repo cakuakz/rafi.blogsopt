aa2f287d78bc21602d4af87e1928a347cdaced9f
cakuakz <rafiirvansyah88@gmail.com>
2024-12-02 01:47:05 +0700

    feat: adding sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,8 +9,10 @@ import mdx from '@astrojs/mdx';
 
 import react from '@astrojs/react';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
   site: process.env.SITE_URL || 'http://localhost:4321',
-  integrations: [tailwind(), icon(), mdx(), react()]
+  integrations: [tailwind(), icon(), mdx(), react(), sitemap()]
 });
