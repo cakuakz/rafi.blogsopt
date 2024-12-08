2cb89c851fea30c03625a306758ebc1453378037
cakuakz <rafiirvansyah88@gmail.com>
2024-12-08 21:01:58 +0700

    feat: adding home banner and change font

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -8,7 +8,10 @@ export default {
 	  extend: {
 		dropShadow: {
 			'custom-input': '0px 0px 10px rgba(38, 38, 38, 0.4)',
-		  },
+		},
+		backgroundImage: {
+			'custom-white-gradient': 'linear-gradient(to bottom, white 0%, white 40%, white 40%, white 40%, white 90%)',
+		},
 	  },
 	},
 	plugins: [],
